24b8b72ed682207196c82f4cc497f7ff0bbc5204
nageetaw <nigeetawadhwani@gmail.com>
2026-01-03 21:48:03 +0100

    Fix routing: Switch to HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import CV from './pages/CV'
 import Gallery from './pages/Gallery'
